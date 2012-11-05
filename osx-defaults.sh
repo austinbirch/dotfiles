a7337fd7aa35339d2dd1c9a232d7fd9de1a305a7
Austin Birch <mraustinbirch@gmail.com>
2012-11-05 22:57:19 +0000

    We don't need to be able to quit the Finder

--- osx-defaults.sh
+++ osx-defaults.sh
@@ -37,7 +37,7 @@ defaults write com.apple.dock no-glass -bool true
 # defaults write NSGlobalDomain AppleShowScrollBars -string "Auto"
 
 echo "Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons"
-defaults write com.apple.finder QuitMenuItem -bool true
+# defaults write com.apple.finder QuitMenuItem -bool true
 
 # Disable window animations and Get Info animations in Finder
 # defaults write com.apple.finder DisableAllAnimations -bool true
