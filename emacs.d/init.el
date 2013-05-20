bc0a97dc56689c146eef8154f9077b8bebdd8ad7
Austin Birch <mraustinbirch@gmail.com>
2013-05-20 10:45:41 +0100

    Word wrap and (commented-out) session saving for emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -157,6 +157,22 @@
 (require 'coffee-mode)
 (custom-set-variables '(coffee-tab-width 2))
 
+;; auto fill settings (word wrap) -
+;; we want it on by default, and only for code comments
+(setq-default auto-fill-function 'do-auto-fill)
+(setq comment-auto-fill-only-comments t)
+
+;; ;; emacs desktop (session) saving
+;; ;; Automatically save and restore sessions
+;; (setq desktop-dirname             "~/.emacs.d/desktop/"
+;;       desktop-base-file-name      "emacs.desktop"
+;;       desktop-base-lock-name      "lock"
+;;       desktop-path                (list desktop-dirname)
+;;       desktop-save                t
+;;       desktop-files-not-to-save   "^$" ;reload tramp paths
+;;       desktop-load-locked-desktop nil)
+;; (desktop-save-mode 1)
+
 ;; handlebars
 (require 'handlebars-mode)
 
